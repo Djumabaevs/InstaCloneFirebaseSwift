d672aaa6bd43a64628e41f7570606606da595f2e
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-06 10:26:12 +0600

    add elements to view controller class

--- InstaCloneFirebaseSwift/ViewController.swift
+++ InstaCloneFirebaseSwift/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+    }
 }
 
