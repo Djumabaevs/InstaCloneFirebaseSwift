80b3b4ed8ba9afba75d29f7d50886d957b2b8a8a
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-09 22:46:57 +0600

    add ui to upload storyboard

--- InstaCloneFirebaseSwift/UploadViewController.swift
+++ InstaCloneFirebaseSwift/UploadViewController.swift
@@ -9,21 +9,20 @@ import UIKit
 
 class UploadViewController: UIViewController {
 
+    @IBOutlet weak var commentText: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var uploadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+ 
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func uploadButtonClicked(_ sender: Any) {
+        
     }
-    */
+    
+   
 
 }
