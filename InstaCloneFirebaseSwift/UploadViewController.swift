0fdf7b29231a6309ecef993aa2eb36beb8195285
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-11 14:24:43 +0600

    storage of firebase storage reference

--- InstaCloneFirebaseSwift/UploadViewController.swift
+++ InstaCloneFirebaseSwift/UploadViewController.swift
@@ -6,8 +6,9 @@
 //
 
 import UIKit
+import Firebase
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var commentText: UITextField!
     @IBOutlet weak var imageView: UIImageView!
@@ -15,11 +16,30 @@ class UploadViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
  
     }
     
+    @objc func chooseImage() {
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
     @IBAction func uploadButtonClicked(_ sender: Any) {
+        
+        let storage = Storage.storage()
+        let storagReference = storage.reference()
         
     }
     
