6110791197a32414785fdc1db8edb5c4c06629d0
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-13 12:59:01 +0600

    cell for row and number of rows for funcs in table view

--- InstaCloneFirebaseSwift/FeedViewController.swift
+++ InstaCloneFirebaseSwift/FeedViewController.swift
@@ -7,17 +7,25 @@
 
 import UIKit
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.delegate = self
+        tableView.dataSource = self
         
     }
     
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
     
 
 }
