ea67ed0bb86685b036446ae74f33a09832e97841
BakytDzhumabev <djumabaevb@gmail.com>
2021-08-13 12:40:50 +0600

    add cocoa touch class for feed cell

---
+++ InstaCloneFirebaseSwift/FeedCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FeedCellTableViewCell.swift
+//  InstaCloneFirebaseSwift
+//
+//  Created by Bakyt Dzhumabaev on 13/8/21.
+//
+
+import UIKit
+
+class FeedCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
